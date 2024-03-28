1caac535bfec31ba4126aab9eee53f5c9445f8bb
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2024-03-28 20:40:28 +0530

    challenge 4

--- challenge 3.js
+++ challenge 3.js
@@ -1,12 +1,12 @@
 /* Write your code below. Good luck! 🙂 */
 
-const scoreDolphins = (96+108+89)/3;
-const scoreKoalas =(88+91+110)/3;
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
 
-if(scoreDolphins===scoreKoalas){
+if (scoreDolphins === scoreKoalas) {
     console.log("Both win the trophy");
-}else if(scoreDolphins>scoreKoalas){
+} else if (scoreDolphins > scoreKoalas) {
     console.log("Dolphins win the trophy");
-}else{
+} else {
     console.log("Koalas win the trophy");
 }
---
+++ challenge 4.js
@@ -0,0 +1,8 @@
+const bill = 275;
+
+/* Write your code below. Good luck! 🙂 */
+let tip = 0;
+
+(bill >= 50 && bill <= 300) ? tip = bill * 0.15 : tip = bill * 0.2;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
