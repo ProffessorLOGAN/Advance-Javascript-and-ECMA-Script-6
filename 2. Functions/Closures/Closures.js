e9935ca4ab10ae9bffcf698fe9a8164e44f7cb68
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2024-05-15 16:21:03 +0530

    closures in javascript

---
+++ 2. Functions/Closures/Closures.js
@@ -0,0 +1,40 @@
+const secureBooking = function () {
+    let passengerCount = 0;
+
+    return function(){
+        passengerCount++;
+        console.log(`${passengerCount} passengers`);
+    }
+}
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+// example 1
+
+let f;
+
+const g =  function(){
+    const a=23;
+    f = function(){
+        console.log(a*2);
+    }
+}
+
+const h = function(){
+    const b = 777;
+    f = function(){
+        console.log(b*2);
+    }
+}
+
+g();
+f();
+console.dir(f);
+//re- assigning f function
+h();
+f();
+console.log(f);
